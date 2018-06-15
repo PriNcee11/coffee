227464aa34bf5cbefb5a3634f439e8773380883f
imartin <imartin@bitban.com>
2018-06-15 15:25:10 +0200

    Implementamos las librerías

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+import 'vue-awesome/icons'
+import 'vuetify/dist/vuetify.min.css'
+
+
+Vue.use(VueRouter)
 
 new Vue({
   el: '#app',
